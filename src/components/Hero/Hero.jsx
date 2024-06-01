8792e435ee2b272cf99f88891a6b5e2f6ae5ef57
HAREESHRAVI <hareesh.unofficial@gmail.com>
2024-06-01 23:39:44 +0530

    refactor hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,14 @@ import heroppp from '../../assets/images/heroppp.jpg'
 const Hero = () => {
   return (
     <>
-    <div className='hero-section'>
-      <span className="hero-content">Unlock your Potential with<br/> <span className='title'>PathPlanPro</span></span>
-      <span className='title-description'>Discover the step-by-step roadmaps to master new technologies <br/>and skills.</span>
-      <div className="hero-buttons">
-        <button className='explore-button'>Explore</button>
-        <button className='signup-button'>Sign Up</button>
+    <div className='hero'>
+      <div className="hero-section">
+        <span className="hero-content">Unlock your Potential with<br/> <span className='title'>PathPlanPro</span></span>
+        <span className='title-description'>Discover the step-by-step roadmaps to master new technologies <br/>and skills.</span>
+        <div className="hero-buttons">
+          <button className='explore-button'>Explore</button>
+          <button className='signup-button'>Sign Up</button>
+        </div>
       </div>
       <img className='hero-image' src={heroppp} alt="Hero-image" />
     </div>
