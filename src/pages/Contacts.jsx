67db77d6f3b0ce49a16a11286ae3913bcd8fc588
HAREESHRAVI <hareesh.unofficial@gmail.com>
2024-06-03 21:34:08 +0530

    added Contact page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar1 from '../components/Navbar1/Navbar1'
 import CHeader from '../components/CHeader/CHeader'
 import Contact from '../components/Contact/Contact'
+import Testimonials from '../components/Testimonials/Testimonials'
+import Footer from '../components/Footer/Footer'
 
 const Contacts = () => {
   return (
@@ -9,6 +11,8 @@ const Contacts = () => {
     <Navbar1/>
     <CHeader/>
     <Contact/>
+    <Testimonials/>
+    <Footer/>
     </>
   )
 }
